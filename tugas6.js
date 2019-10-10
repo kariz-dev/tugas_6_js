83544d6af9ca35c6c5e4ef62461f6fa4cb374db7
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-10 20:21:11 +0700

    tugas 6 javascript

---
+++ tugas6.js
@@ -0,0 +1,12 @@
+/* tugas 6 javascript */
+
+var hasil = "";
+for(var i=1; i<=20; i++){
+    if(i % 2 === 0){
+        hasil += "genap "
+    }else{
+        hasil += i + " ";
+    }
+}
+
+console.log(hasil);
